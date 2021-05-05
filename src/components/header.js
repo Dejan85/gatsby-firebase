aa35ac6dd795aad55cf379d3d9fa7abf302e4098
Dejan85 <xypnotica@gmail.com>
2021-05-05 22:10:01 +0200

    created register page

--- src/components/header.js
+++ src/components/header.js
@@ -52,6 +52,12 @@ const LoginLink = styled.div`
   }
 `;
 
+export const Divider = styled.span`
+  margin: 0 8px;
+  padding-right: 1px;
+  background: #ddd;
+`;
+
 const Header = ({ siteTitle }) => {
   const { firebase, user } = useContext(FirebaseContext);
 
@@ -79,6 +85,8 @@ const Header = ({ siteTitle }) => {
           {(!user || !user.email) && (
             <LoginLink>
               <Link to="/login">Login</Link>
+              <Divider />
+              <Link to="/register">Register</Link>
             </LoginLink>
           )}
         </div>
--- src/pages/register.js
+++ src/pages/register.js
@@ -2,7 +2,25 @@ import React from "react";
 import { Form, Input, Button } from "../components/common";
 
 const Register = () => {
-  return <Form>test</Form>;
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <Input placeholder="email" type="email" required />
+      <Input placeholder="password" type="password" required minLength={3} />
+      <Input
+        placeholder="confirm password"
+        type="password"
+        required
+        minLength={3}
+      />
+      <Button type="submit" block>
+        Register
+      </Button>
+    </Form>
+  );
 };
 
 export default Register;
