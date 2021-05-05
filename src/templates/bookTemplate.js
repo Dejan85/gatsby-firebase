e775bc8d60bb80619066053dc6234f2842e3b9f3
Dejan85 <xypnotica@gmail.com>
2021-05-05 15:12:37 +0200

    created bookTemplate page

--- src/templates/bookTemplate.js
+++ src/templates/bookTemplate.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Layout from "../components/layout";
 
-const BookTemplate = () => {
-  return <div>BOOK PAGE</div>;
+const BookTemplate = props => {
+  const {
+    pageContext: {
+      title,
+      summary,
+      author: { name },
+    },
+  } = props;
+
+  return (
+    <Layout>
+      <section>
+        <h2>
+          {title} - <small>{name}</small>
+        </h2>
+        <p>{summary}</p>
+      </section>
+    </Layout>
+  );
 };
 
 export default BookTemplate;
