9eb39b1c278c07ebb40e8e05aaa38c3871ac656b
Dejan85 <xypnotica@gmail.com>
2021-05-05 22:16:32 +0200

    created register page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Button } from "../components/common";
 
 const Register = () => {
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const onSubmit = e => {
     e.preventDefault();
+
+    console.log("test", formValues);
+  };
+
+  const onChange = e => {
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   return (
     <Form onSubmit={onSubmit}>
-      <Input placeholder="email" type="email" required />
-      <Input placeholder="password" type="password" required minLength={3} />
       <Input
+        onChange={onChange}
+        placeholder="email"
+        type="email"
+        name="email"
+        value={formValues.email}
+        required
+      />
+      <Input
+        onChange={onChange}
+        placeholder="password"
+        type="password"
+        required
+        minLength={3}
+        name="password"
+        value={formValues.password}
+      />
+      <Input
+        onChange={onChange}
         placeholder="confirm password"
         type="password"
         required
         minLength={3}
+        name="confirmPassword"
+        value={formValues.confirmPassword}
       />
       <Button type="submit" block>
         Register
