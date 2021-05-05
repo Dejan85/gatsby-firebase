fc15e652b808287949a38050102f871c32a96de6
Dejan85 <xypnotica@gmail.com>
2021-05-05 19:06:14 +0200

    firebase wepack config

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,3 +27,19 @@ exports.createPages = async ({ graphql, actions }) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  if (stage === "develop-html" || stage === "build-html") {
+    actions.setWebpackConfig({
+      resolve: {
+        mainFields: ["main"],
+      },
+    });
+  } else {
+    actions.setWebpackConfig({
+      resolve: {
+        mainFields: ["browser", "module", "main"],
+      },
+    });
+  }
+};
