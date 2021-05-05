d18df3dd9f5dad2af2ba2362e9146fbf1ab4548e
Dejan85 <xypnotica@gmail.com>
2021-05-05 14:51:10 +0200

    created book page programaticly

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,34 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const bookTemplate = path.resolve("src/templates/bookTemplate.js");
+  const {
+    data: {
+      allBook: { edges },
+    },
+  } = await graphql(`
+    {
+      allBook {
+        edges {
+          node {
+            id
+            title
+            summary
+            author {
+              name
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  edges.forEach(book => {
+    createPage({
+      path: `/book/${book.node.id}`,
+      component: bookTemplate,
+      context: book.node,
+    });
+  });
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,56 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { Link, graphql } from "gatsby";
+// import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+// import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = props => {
+  const {
+    data: {
+      allBook: { edges },
+    },
+  } = props;
 
-export default IndexPage
+  return (
+    <Layout>
+      {edges.map(book => {
+        const {
+          node: {
+            title,
+            id,
+            summary,
+            author: { name },
+          },
+        } = book;
+
+        return (
+          <div key={id}>
+            <h2>{title}</h2> - <small>{name}</small>
+            <div>{summary}</div>
+            <Link to={`/book/${id}`}>Join coversation</Link>
+          </div>
+        );
+      })}
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    allBook {
+      edges {
+        node {
+          id
+          title
+          summary
+          author {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default IndexPage;
